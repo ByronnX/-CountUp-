8b936c13789a29115a4028a24784e51fdd7acd14
xyh20001018 <1031466692@qq.com>
2023-04-04 14:17:51 +0800

    自定义CountUp组件

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <vue-count-up :end="2500" :duration="2.5"></vue-count-up>
+</template>
+
+<script>
+import { CountUp } from 'countup.js'
+import VueCountUp from './VueCountUp.vue';
+export default {
+  name: 'countup-remake',
+  components: {
+    VueCountUp
+  },
+  data() {
+
+  },
+  mounted() {
+  },
+  methods: {
+
+  },
+  props: {
+
+  }
+}
+</script>
---
+++ src/VueCountUp.vue
@@ -0,0 +1,56 @@
+<template>
+    <span ref="countup"></span>
+</template>
+
+<script>
+import { CountUp } from 'countup.js'
+export default {
+    name: 'VueCountUp',
+    data() {
+        return {
+            numAnim: null
+        }
+    },
+    mounted() {
+        this.initCountUp()
+    },
+    computed: {
+        options() {
+            return {
+                startVal: this.start,
+                decimal: this.decimal,
+                duration: this.duration
+            }
+        }
+    },
+    methods: {
+        initCountUp() {
+            this.numAnim = new CountUp(this.$refs.countup,
+                this.end,
+                this.options)
+            this.numAnim.start()
+        }
+    },
+    props: {
+        start: {
+            type: Number,
+            default: 0
+        },
+        end: {
+            type: Number,
+            default: 2017
+        },
+        decimal: {
+            type: Number,
+            default: 0
+        },
+        duration: {
+            type: Number,
+            default: 2.5
+        },
+        options: {
+            type: Object
+        }
+    }
+}
+</script>
